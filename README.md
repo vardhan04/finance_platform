b93887a08421821f75566ec2123d3bb2bc54d261
vardhan04 <saivardhan18@gmail.com>
2025-01-20 12:11:01 -0800

    final commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Cashquix: Take Control of Your Finances with Smart Solutions
+### Make sure to create a `.env` file with following -
+
+```
+DATABASE_URL=
+DIRECT_URL=
+
+NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY=
+CLERK_SECRET_KEY=
+GEMINI_API_KEY=
+RESEND_API_KEY=
+ARCJET_KEY=
+```
---
+++ data/landing.js
@@ -0,0 +1,67 @@
+import {
+  BarChart3,
+  Receipt,
+  PieChart,
+  CreditCard,
+  Globe,
+  Zap,
+} from "lucide-react";
+
+
+// Features Data
+export const featuresData = [
+  {
+    icon: <BarChart3 className="h-8 w-8 text-blue-600" />,
+    title: "Advanced Insights",
+    description:
+      "Analyze your spending habits with AI-driven analytics for smarter decisions.",
+  },
+  {
+    icon: <Receipt className="h-8 w-8 text-blue-600" />,
+    title: "Smart Receipt Reader",
+    description:
+      "Easily extract key details from receipts using cutting-edge AI technology.",
+  },
+  {
+    icon: <PieChart className="h-8 w-8 text-blue-600" />,
+    title: "Intelligent Budgeting",
+    description: "Plan and track your budgets with personalized recommendations.",
+  },
+  {
+    icon: <CreditCard className="h-8 w-8 text-blue-600" />,
+    title: "Account Aggregation",
+    description: "Consolidate multiple accounts and cards into one streamlined platform.",
+  },
+  {
+    icon: <Globe className="h-8 w-8 text-blue-600" />,
+    title: "Global Currency Support",
+    description: "Seamlessly manage multiple currencies with live exchange rates.",
+  },
+  {
+    icon: <Zap className="h-8 w-8 text-blue-600" />,
+    title: "Automated Financial Tips",
+    description: "Receive personalized, real-time financial advice and insights.",
+  },
+];
+
+// How It Works Data
+export const howItWorksData = [
+  {
+    icon: <CreditCard className="h-8 w-8 text-blue-600" />,
+    title: "1. Create Your Account",
+    description:
+      "Get started in minutes with our simple and secure sign-up process",
+  },
+  {
+    icon: <BarChart3 className="h-8 w-8 text-blue-600" />,
+    title: "2. Track Your Spending",
+    description:
+      "Automatically categorize and track your transactions in real-time",
+  },
+  {
+    icon: <PieChart className="h-8 w-8 text-blue-600" />,
+    title: "3. Get Insights",
+    description:
+      "Receive AI-powered insights and recommendations to optimize your finances",
+  },
+];
